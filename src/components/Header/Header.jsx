26fa429c7ad79795ba7242be3ef167ba9f3e4080
Nikita Melnikov <nik.y.melnikov@gmail.com>
2020-02-27 13:41:11 +0000

    Added a profile picture to the main page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Panel from '../Panel/Panel';
+import ProfilePicture from '../../images/pp300.png';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
@@ -12,12 +13,23 @@ const useStyles = makeStyles((theme) => ({
       alignItems: 'center',
     },
   },
+  heading: {
+    display: 'flex',
+    justifyContent: 'center',
+    marginTop: '12px',
+  },
   title: {
     ...theme.typography.h1,
     color: theme.palette.colors.coreWhite,
-    margin: 0,
-    margin: '20px 0',
+    position: 'absolute',
+    top: 150,
     textShadow: `5px 5px 0px ${theme.palette.colors.coreTeal}, -5px -5px 0px ${theme.palette.colors.coreRed}`,
+  },
+  image: {
+    borderRadius: '50%',
+    width: '200px',
+    marginBottom: '20px',
+    border: `3px solid ${theme.palette.colors.coreRed}`,
   },
   nav: {
     display: 'flex',
@@ -46,7 +58,10 @@ const Header = () => {
 
   return (
     <div className={classes.root}>
-      <h1 className={classes.title}>Nik Melnikov</h1>
+      <div id="picture-title" className={classes.heading}>
+        <img className={classes.image} src={ProfilePicture}/>
+        <h1 className={classes.title}>Nik Melnikov</h1>
+      </div>
       <Panel className={classes.nav}>
         <a href="#" className={classes.navItem}>Who</a>
         <a href="#" className={classes.navItem}>Projects</a>
