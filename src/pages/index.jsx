2f4b307b050889500395b230d2f669d89b658beb
Nikita Melnikov <nik.y.melnikov@gmail.com>
2020-02-27 12:16:51 +0000

    Added some placeholder text for a front page panel

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,23 @@
 import React from "react";
-import Layout from "../components/Layout/Layout";
+import Grid from '@material-ui/core/Grid';
+import Panel from '../components/Panel/Panel';
+import { makeStyles } from '@material-ui/core/styles';
 
 const IndexPage = () => (
-  <div>
-    <p>Test</p>
-  </div>
+  <Grid container>
+    <Grid item md={6} xs={12}>
+      <Panel>
+        <p>Hi, I'm Nikita (or Nik). I'm a computer science student with a passion for
+           programming and the subject as a whole, with interests in online technologies
+           and web development.
+          <br/><br/>
+          I'm always looking for a challenge to help me build on and practice my skills,
+           and enjoy working on projects from time to time. If you're interested in
+            having me help work on your project(s), don't hesitate to contact me and
+            I'm sure we can work something out!</p>
+      </Panel>
+    </Grid>
+  </Grid>
 )
 
 export default IndexPage
